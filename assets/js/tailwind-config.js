7f8ec59d160687962615a9f0fb3533c336e5c383
Kim seunghyun <ksh10170505@gmail.com>
2026-01-13 03:26:33 +0900

    inex 수정

--- assets/js/tailwind-config.js
+++ assets/js/tailwind-config.js
@@ -2,9 +2,11 @@ tailwind.config = {
   theme: {
     extend: {
       fontFamily: {
-        'paperozi': ['Paperozi', 'Noto Sans KR', 'sans-serif'],
-        'space': ['Space Mono', 'Noto Sans KR', 'monospace']
-      },
+          // 'sans'를 기본으로 설정하면 모든 글자가 프리텐다드로 변해!
+          'sans': ['Pretendard', 'Noto Sans KR', 'sans-serif'],
+          'paperozi': ['Paperozi', 'Noto Sans KR', 'sans-serif'],
+          'space': ['Space Mono', 'Noto Sans KR', 'monospace']
+        },
       screens: {
         '3xl': '2561px', // 쉼표와 따옴표 위치 확인!
       }
